3c2ff89e1a3de64818f6c942c93f3c9f03491632
VincentHChoy <vhchoy@ualberta.ca>
2022-11-16 11:46:57 -0700

    final app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Skills from "./components/Skills";
+import Projects from "./components/Projects";
+import Typewriter from "./components/Typewriter";
+import Education from "./components/Education";
+import Contact from "./components/Contact";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="h-screen overflow-x-hidden bg-gradient-to-t from-third to-primary text-white">
+      <Navbar />
+      <Typewriter />
+      <Skills />
+      <Projects />
+      <Education />
+      <Contact/>
     </div>
   );
 }
