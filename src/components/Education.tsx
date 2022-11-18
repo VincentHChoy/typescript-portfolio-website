0c20411656fa77f5c0dd69758fd2f5eccc0f3aac
VincentHChoy <vhchoy@ualberta.ca>
2022-11-18 15:33:13 -0700

    add new engineering icon

--- src/components/Education.tsx
+++ src/components/Education.tsx
@@ -4,7 +4,7 @@ function Education() {
   const projects = [
     {
       name: "University of Alberta",
-      preview: "https://sites.ualberta.ca/~dnobes/img/Logo_Engg2.png",
+      preview: "Logo_Engg2.png",
       link: "https://www.ualberta.ca/engineering/civil-environmental-engineering/index.html",
       desc: "Bachelors of Civil Engineering Co-op Program 2020",
       stack: ["HTML5", "CSS5", "Javascript", "React", "Tailwind"],
@@ -22,12 +22,14 @@ function Education() {
   const educationContainers = () => {
     return projects.map((project) => {
       return (
-        <main className="mx-auto my-4 flex h-2/3 lg:w-1/3 w-5/6 flex-col items-center justify-center rounded-md bg-gradient-to-t from-secondary to-primary p-10 shadow-xl lg:mx-4 lg:items-center lg:justify-between">
+        <main className="mx-auto my-4 flex h-2/3 w-5/6 flex-col items-center justify-center rounded-md bg-gray-400 bg-opacity-30 p-10 shadow-xl lg:mx-4 lg:w-1/3 lg:items-center lg:justify-between">
           <h1 className="text-3xl font-bold">{project.name}</h1>
           <a href={project.link} target="_blank">
             <img className="my-4 w-fit rounded-md" src={project.preview} />
           </a>
-          <p className="text-2xl lg:text-3xl">{project.desc}</p>
+          <p className="break-normal text-2xl font-bold lg:text-xl">
+            {project.desc}
+          </p>
           {/* <section className="my-2 flex w-full flex-row flex-wrap items-start justify-center"></section>
           <footer></footer> */}
         </main>
@@ -41,7 +43,9 @@ function Education() {
     <section className="mt-8" id="education">
       <Fade>
         <div className="flex flex-col items-center justify-around lg:h-screen">
-          <h1 className="break-words text-center text-4xl">Education</h1>
+          <h1 className="break-words text-center font-mont text-4xl font-bold">
+            Education
+          </h1>
           <main className="flex h-5/6 flex-col justify-center lg:w-5/6 lg:flex-row lg:items-start">
             {containers}
           </main>
