046383e02fd813bd9a9a6c0bc124fedba025a8d1
VincentHChoy <vhchoy@ualberta.ca>
2023-01-12 13:31:18 -0700

    add picture to contact and title

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -15,10 +15,10 @@
 //             Contact Me!
 //           </h1>
 //         </a>
-//         <img
-//           className="w-5/6 rounded-full shadow-2xl lg:w-1/6"
-//           src={"/me.jpg"}
-//         />
+// <img
+//   className="w-5/6 rounded-full shadow-2xl lg:w-1/6"
+//   src={"/me.jpg"}
+// />
 //         <ul className="w-full p-10 mx-auto text-center lg:w-1/2 lg:text-2xl">
 //           <li className="flex flex-row items-start justify-center">
 //             <a>(780) 200-5180</a>
@@ -59,39 +59,49 @@ function Contact() {
   }
 
   return (
-    <section className="mx-auto mb-32 flex h-1/2 w-4/5 flex-col items-center justify-center rounded-md border-b-4 border-b-white bg-[#3e363f] bg-opacity-80 p-10 shadow-xl">
-      <form
-        action="https://formspree.io/f/xyyaylva"
-        className="flex h-full w-full flex-col justify-around"
-        onSubmit={handleSubmit}
-      >
-        <label htmlFor="email">Email Address</label>
-        <input
-          className="rounded-md p-2"
-          id="email"
-          type="email"
-          name="email"
-        />
-        <ValidationError prefix="Email" field="email" errors={state.errors} />
-        <textarea
-          className="h-1/2 w-full rounded-md p-2 text-black"
-          id="message"
-          name="message"
-        />
-        <ValidationError
-          prefix="Message"
-          field="message"
-          errors={state.errors}
-        />
-        <button
-          className="text-opacity-0.5 h-1/6 lg:w-1/6 rounded-md bg-white p-2 text-secondary hover:bg-secondary hover:text-white"
-          type="submit"
-          disabled={state.submitting}
+    <>
+      <h1 className="my-5 break-words text-center font-mont text-4xl font-bold">
+        Contact Me!
+      </h1>
+      <img
+        className="mx-auto my-10 w-5/6 rounded-full shadow-2xl shadow-white/30 border-4 lg:w-1/6"
+        src={"/me.jpg"}
+      />
+      <section className="mx-auto mb-32 flex h-1/2 w-4/5 flex-col items-center justify-center rounded-md border-b-4 border-b-white bg-[#3e363f] bg-opacity-80 p-10 shadow-xl">
+        <form
+          action="https://formspree.io/f/xyyaylva"
+          className="flex h-full w-full flex-col justify-around"
+          onSubmit={handleSubmit}
         >
-          Submit
-        </button>
-      </form>
-    </section>
+          <label htmlFor="email">Email Address:</label>
+          <input
+            className="rounded-md p-2"
+            id="email"
+            type="email"
+            name="email"
+          />
+          <ValidationError prefix="Email" field="email" errors={state.errors} />
+          <label>Message:</label>
+          <textarea
+            className="h-1/2 w-full rounded-md p-2 text-black"
+            id="message"
+            name="message"
+          />
+          <ValidationError
+            prefix="Message"
+            field="message"
+            errors={state.errors}
+          />
+          <button
+            className="text-opacity-0.5 my-3 h-1/6 rounded-md bg-white p-2 text-secondary hover:bg-secondary hover:text-white lg:w-1/6"
+            type="submit"
+            disabled={state.submitting}
+          >
+            Send
+          </button>
+        </form>
+      </section>
+    </>
   );
 }
 export default Contact;
